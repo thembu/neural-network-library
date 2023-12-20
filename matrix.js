f77d33dfe4c06b2214e7b8f9e4adaa3b33911b9c
thembu <rakhivhanithembu@gmail.com>
2023-12-20 22:30:20 +0200

    added matrix multilication

--- matrix.js
+++ matrix.js
@@ -51,10 +51,40 @@ add = function(n) {
 }
 
 multiply = function(n) {
+
+    if(n instanceof Matrix) {
+        if(this.cols !== n.rows) {
+            console.log("Cols of A must match rows of B");
+            return undefined;
+        }
+
+       let a = this.matrix;
+       let b = n.matrix;
+
+       let result = new Matrix(a.rows , b.cols); 
+
+       
+       for(let i = 0; i < result.rows ; i++) {
+        for (let j = 0; j < result.cols; j++) {
+              // dot roduct of values in col
+              let sum = 0
+             for(let k = 0; k < a.cols; k++) {
+                sum += a.matrix[i][k] * b.matrix[k][j];
+             }
+
+              result[i][j] = sum;
+        }
+       }
+       
+
+    } 
+    else {
+
     for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols ; j++) {
             this.matrix[i][j] *= n;
         }
     }
  }
+}
 }
